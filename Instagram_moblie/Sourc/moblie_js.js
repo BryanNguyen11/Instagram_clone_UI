a7ca1c9fa7a0e932eec80ef1a4cde3fe07482743
tak1911 <huyhoangg191103@gmail.com>
2023-05-03 21:15:29 +0700

    Posts event

--- Instagram_moblie/Sourc/moblie_js.js
+++ Instagram_moblie/Sourc/moblie_js.js
@@ -3,3 +3,28 @@ const notificationButton=document.querySelector("#header_right_item_notification
 notificationButton.addEventListener("click",()=>{
     print ("hi"); 
 })
+
+function flike(){
+    const like = document.querySelector(".btnLike");
+    if(like.src.match("../icons/like.png")){
+        like.src = "../icons/liked.png";
+        document.querySelector(".likes").innerHTML = "1 likes";
+    }else{
+        like.src="../icons/like.png";
+        document.querySelector(".likes").innerHTML ="0 likes";
+    }
+}
+
+function fsave(){
+    const save = document.querySelector(".btnSave");
+    if(save.src.match("../icons/save-instagram.png")){
+        save.src = "../icons/saved-instagram.png";
+    }else{
+        save.src="../icons/save-instagram.png";
+        
+    }
+}
+
+function fcomment(){
+    document.getElementById("add-comment").focus();
+}
