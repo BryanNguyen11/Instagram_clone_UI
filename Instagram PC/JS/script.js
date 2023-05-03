0d03f4768ca6ab9ec4abf7fc231be958c93ade55
TriTin <tinnguyen5071@gmail.com>
2023-05-03 21:06:12 +0700

    update icons

--- Instagram PC/JS/script.js
+++ Instagram PC/JS/script.js
@@ -4,14 +4,14 @@ var check = true
 function love() {
         var checked = document.getElementById("buttonLove")
         if (check == true) {
-                checked.src = "../Hinh anh/Icon/redHeart.png"
-                checked.style = "width: 50px; height: 45px; margin-left: -10px"
+                checked.src = "../Hinh anh/Icon/liked.png"
+                checked.style = "width: 30px; height: 30px; margin-left: -8px; margin-top: 3px"
                 document.getElementById("numOfLove").innerHTML = "101" + " lượt thích"
                 check = false
         }
         else {
                 checked.src = "../Hinh anh/Icon/heart.png"
-                checked.style = "margin-top: -8px; margin-left: -14px; width: 73px; height: 53px"
+                checked.style = " margin-left: -8px; margin-top: 3px; width: 30px; height: 30px"
                 document.getElementById("numOfLove").innerHTML = "100" + " lượt thích"
                 check = true
         }
@@ -20,7 +20,7 @@ function love() {
 /* ============================================================Reels============================================================ */
 var i = 1
 function heartActive() {
-        document.getElementById("changeImage").src = "../Hinh anh/Icon/redHeart.png"
-        document.getElementById("changeImage").style = "width: 55px; height: 45px; margin-top: 10px; margin-left: -3px"
+        document.getElementById("changeImage").src = "../Hinh anh/Icon/liked.png"
+        document.getElementById("changeImage").style = "width: 35px; height: 35px;"
         document.getElementById("numOfLove").innerHTML = i++
 }
