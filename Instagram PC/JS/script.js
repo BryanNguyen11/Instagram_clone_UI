31ea51f9570b23737c76185c61885b83c880452b
TriTin <tinnguyen5071@gmail.com>
2023-05-07 16:18:13 +0700

    Them cac bai post

--- Instagram PC/JS/script.js
+++ Instagram PC/JS/script.js
@@ -1,22 +1,49 @@
 /* ============================================================Home============================================================ */
 
 var check = true
-function love() {
-        var checked = document.getElementById("buttonLove")
+function love(obj) {
         if (check == true) {
-                checked.src = "../Hinh anh/Icon/liked.png"
-                checked.style = "width: 30px; height: 30px; margin-left: -8px; margin-top: 3px"
-                document.getElementById("numOfLove").innerHTML = "101" + " lượt thích"
+                obj.src = "../Hinh anh/Icon/liked.png"
+                obj.style = "width: 30px; height: 30px; margin-top: 3px; cursor: pointer;"
+                if (obj == document.getElementById("buttonLove2")) {
+                        document.getElementById("numOfLove2").innerHTML = "318,059 lượt thích"
+                }
+                else if (obj == document.getElementById("buttonLove1")) {
+                        document.getElementById("numOfLove1").innerHTML = "101 lượt thích"
+                }
+                else if (obj == document.getElementById("buttonLove3")) {
+                        document.getElementById("numOfLove3").innerHTML = "5,856,936 lượt thích"
+                }
+
                 check = false
         }
         else {
-                checked.src = "../Hinh anh/Icon/heart.png"
-                checked.style = " margin-left: -8px; margin-top: 3px; width: 30px; height: 30px"
-                document.getElementById("numOfLove").innerHTML = "100" + " lượt thích"
+                obj.src = "../Hinh anh/Icon/heart.png"
+                obj.style = "margin-top: 3px; width: 30px; height: 30px; cursor: pointer;"
+                if (obj == document.getElementById("buttonLove2")) {
+                        document.getElementById("numOfLove2").innerHTML = "318,058 lượt thích"
+                }
+                else if (obj == document.getElementById("buttonLove1")) {
+                        document.getElementById("numOfLove1").innerHTML = "100 lượt thích"
+                }
+                else if (obj == document.getElementById("buttonLove3")) {
+                        document.getElementById("numOfLove3").innerHTML = "5,856,935 lượt thích"
+                }
                 check = true
         }
 }
 
+var checkLikeComment = true
+function likeComment(obj) {
+        if (checkLikeComment == true) {
+                obj.src = "../Hinh anh/Icon/redHeart.png"
+                checkLikeComment = false
+        }
+        else if (checkLikeComment == false) {
+                obj.src = "../Hinh anh/Icon/heart.png"
+                checkLikeComment = true
+        }
+}
 /* ============================================================Reels============================================================ */
 var i = 1
 function heartActive() {
@@ -24,3 +51,4 @@ function heartActive() {
         document.getElementById("changeImage").style = "width: 35px; height: 35px;"
         document.getElementById("numOfLove").innerHTML = i++
 }
+
